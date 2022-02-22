9d493ffc4a2acfe6a17bc1ee8d07c7ced85a52f3
weidenfelix <felix.weidenholzer@gmail.com>
2022-02-22 12:00:37 +0100

    now it only parses the value if it's a string and storing it as type DateTime does not err

--- app.py
+++ app.py
@@ -62,7 +62,10 @@ class Artist(db.Model):
 #----------------------------------------------------------------------------#
 
 def format_datetime(value, format='medium'):
-  date = dateutil.parser.parse(value)
+  if isinstance(value, str):
+    date = dateutil.parser.parse(value)
+  else:
+    date = value
   if format == 'full':
       format="EEEE MMMM, d, y 'at' h:mma"
   elif format == 'medium':
