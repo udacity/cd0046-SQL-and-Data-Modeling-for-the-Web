144996a6d4373aa8f22fe8ad74d55fc60251f7f3
see-why <iyadicy@gmail.com>
2022-05-29 09:27:41 +0100

    import migrate and connect to local database

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import babel
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
@@ -20,6 +21,11 @@ app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:oyinlola@localhost:5432/fyyerApp'
+migrate = Migrate(app, db)
+
 
 # TODO: connect to a local postgresql database
 
