a8b9af38fb00ecdf897722a91ae79ea2f4ccc6ee
murieamalea <aminyaite@gmail.com>
2022-08-21 01:02:20 +0300

    Update requirements.txt

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import logging
 from logging import Formatter, FileHandler
+from sqlalchemy.exc import NoResultFound
 from sqlalchemy_utils.types.encrypted.encrypted_type import dateutil
 
 from forms import *
@@ -101,7 +102,7 @@ class Venue(db.Model):
                 'address': self.address,
                 'image_link': self.image_link,
                 'facebook_link': self.facebook_link,
-                'website': self.website,
+                'website_link': self.website_link,
                 'seeking_talent': self.seeking_talent,
                 'seeking_description': self.seeking_description
                 }
@@ -116,11 +117,11 @@ class Venue(db.Model):
                 'address': self.address,
                 'image_link': self.image_link,
                 'facebook_link': self.facebook_link,
-                'website': self.website,
+                'website_link': self.website_link,
                 'seeking_talent': self.seeking_talent,
                 'seeking_description': self.seeking_description,
                 'num_shows': Show.query.filter(
-                    Show.start_time > datetime.datetime.now(),
+                    Show.start_time > datetime.now(),
                     Show.venue_id == self.id)
                 }
 
@@ -136,18 +137,18 @@ class Venue(db.Model):
                 'facebook_link': self.facebook_link,
                 'seeking_talent': self.seeking_talent,
                 'seeking_description': self.seeking_description,
-                'website': self.website,
+                'website_link': self.website_link,
                 'upcoming_shows': [show.serialize_with_artist_venue for show in Show.query.filter(
-                    Show.start_time > datetime.datetime.now(),
+                    Show.start_time > datetime.now(),
                     Show.venue_id == self.id).all()],
                 'past_shows': [show.serialize_with_artist_venue for show in Show.query.filter(
-                    Show.start_time < datetime.datetime.now(),
+                    Show.start_time < datetime.now(),
                     Show.venue_id == self.id).all()],
                 'upcoming_shows_count': len(Show.query.filter(
-                    Show.start_time > datetime.datetime.now(),
+                    Show.start_time > datetime.now(),
                     Show.venue_id == self.id).all()),
                 'past_shows_count': len(Show.query.filter(
-                    Show.start_time < datetime.datetime.now(),
+                    Show.start_time < datetime.now(),
                     Show.venue_id == self.id).all())
                 }
 
@@ -178,6 +179,61 @@ class Artist(db.Model):
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
+    def add(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.update(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return '<Artist %r>' % self
+
+    @property
+    def serialize_with_shows_details(self):
+        return {'id': self.id,
+                'name': self.name,
+                'city': self.city,
+                'state': self.state,
+                'phone': self.phone,
+                'genres': self.genres,
+                'image_link': self.image_link,
+                'facebook_link': self.facebook_link,
+                'seeking_venue': self.seeking_venue,
+                'seeking_description': self.seeking_description,
+                'website': self.website,
+                'upcoming_shows': [show.serialize_with_artist_venue for show in Show.query.filter(
+                    Show.start_time > datetime.now(),
+                    Show.artist_id == self.id).all()],
+                'past_shows': [show.serialize_with_artist_venue for show in Show.query.filter(
+                    Show.start_time < datetime.now(),
+                    Show.artist_id == self.id).all()],
+                'upcoming_shows_count': len(Show.query.filter(
+                    Show.start_time > datetime.now(),
+                    Show.artist_id == self.id).all()),
+                'past_shows_count': len(Show.query.filter(
+                    Show.start_time < datetime.now(),
+                    Show.artist_id == self.id).all())
+                }
+
+    @property
+    def serialize(self):
+        return {'id': self.id,
+                'name': self.name,
+                'city': self.city,
+                'state': self.state,
+                'phone': self.phone,
+                'genres': self.genres,
+                'image_link': self.image_link,
+                'facebook_link': self.facebook_link,
+                'seeking_venue': self.seeking_venue,
+                }
+
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 class Show(db.Model):
@@ -186,6 +242,7 @@ class Show(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
     venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+    start_time = db.Column(db.DateTime(timezone=False), nullable=False)
 
 
 # Create tables in database
@@ -224,29 +281,29 @@ def index():
 def venues():
     # TODO: replace with real venues data.
     #       num_upcoming_shows should be aggregated based on number of upcoming shows per venue.
-    # data = [{
-    #     "city": "San Francisco",
-    #     "state": "CA",
-    #     "venues": [{
-    #         "id": 1,
-    #         "name": "The Musical Hop",
-    #         "num_upcoming_shows": 0,
-    #     }, {
-    #         "id": 3,
-    #         "name": "Park Square Live Music & Coffee",
-    #         "num_upcoming_shows": 1,
-    #     }]
-    # }, {
-    #     "city": "New York",
-    #     "state": "NY",
-    #     "venues": [{
-    #         "id": 2,
-    #         "name": "The Dueling Pianos Bar",
-    #         "num_upcoming_shows": 0,
-    #     }]
-    # }]
-    data = Venue.query.all()
-    return render_template('pages/venues.html', areas=data);
+    data = []
+    # Obtain list of all unique City & State combination
+    places_query = Venue.query.distinct(Venue.state, Venue.city).all()
+    # Store details of city, state and venues in associative array
+    for place in places_query:
+        places = {
+            "city": place.city,
+            "state": place.state,
+            # Initialize venues list to append the venues to
+            "venues": []
+        }
+
+        venues_info = Venue.query.filter_by(state=place.state, city=place.city).all()
+        for venue in venues_info:
+            places['venues'].append({
+                "id": venue.id,
+                "name": venue.name,
+                "num_upcoming_shows": db.session.query(Show).join(Venue).filter(Show.venue_id == venue.id).filter(
+                    Show.start_time > datetime.now()).count()
+            })
+
+            data.append(places)
+    return render_template('pages/venues.html', areas=data)
 
 
 @app.route('/venues/search', methods=['POST'])
@@ -254,13 +311,13 @@ def search_venues():
     # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
     # seach for Hop should return "The Musical Hop".
     # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
+    search_term = request.form.get('search_term', None)
+    venues_info = Venue.query.filter(
+        Venue.name.ilike("%{}%".format(search_term))).all()
+    count_venues = len(venues_info)
     response = {
-        "count": 1,
-        "data": [{
-            "id": 2,
-            "name": "The Dueling Pianos Bar",
-            "num_upcoming_shows": 0,
-        }]
+        "count": count_venues,
+        "data": [v.serialize for v in venues_info]
     }
     return render_template('pages/search_venues.html', results=response,
                            search_term=request.form.get('search_term', ''))
@@ -270,84 +327,12 @@ def search_venues():
 def show_venue(venue_id):
     # shows the venue page with the given venue_id
     # TODO: replace with real venue data from the venues table, using venue_id
-    data1 = {
-        "id": 1,
-        "name": "The Musical Hop",
-        "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-        "address": "1015 Folsom Street",
-        "city": "San Francisco",
-        "state": "CA",
-        "phone": "123-123-1234",
-        "website": "https://www.themusicalhop.com",
-        "facebook_link": "https://www.facebook.com/TheMusicalHop",
-        "seeking_talent": True,
-        "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-        "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
-        "past_shows": [{
-            "artist_id": 4,
-            "artist_name": "Guns N Petals",
-            "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-            "start_time": "2019-05-21T21:30:00.000Z"
-        }],
-        "upcoming_shows": [],
-        "past_shows_count": 1,
-        "upcoming_shows_count": 0,
-    }
-    data2 = {
-        "id": 2,
-        "name": "The Dueling Pianos Bar",
-        "genres": ["Classical", "R&B", "Hip-Hop"],
-        "address": "335 Delancey Street",
-        "city": "New York",
-        "state": "NY",
-        "phone": "914-003-1132",
-        "website": "https://www.theduelingpianos.com",
-        "facebook_link": "https://www.facebook.com/theduelingpianos",
-        "seeking_talent": False,
-        "image_link": "https://images.unsplash.com/photo-1497032205916-ac775f0649ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
-        "past_shows": [],
-        "upcoming_shows": [],
-        "past_shows_count": 0,
-        "upcoming_shows_count": 0,
-    }
-    data3 = {
-        "id": 3,
-        "name": "Park Square Live Music & Coffee",
-        "genres": ["Rock n Roll", "Jazz", "Classical", "Folk"],
-        "address": "34 Whiskey Moore Ave",
-        "city": "San Francisco",
-        "state": "CA",
-        "phone": "415-000-1234",
-        "website": "https://www.parksquarelivemusicandcoffee.com",
-        "facebook_link": "https://www.facebook.com/ParkSquareLiveMusicAndCoffee",
-        "seeking_talent": False,
-        "image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
-        "past_shows": [{
-            "artist_id": 5,
-            "artist_name": "Matt Quevedo",
-            "artist_image_link": "https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
-            "start_time": "2019-06-15T23:00:00.000Z"
-        }],
-        "upcoming_shows": [{
-            "artist_id": 6,
-            "artist_name": "The Wild Sax Band",
-            "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-            "start_time": "2035-04-01T20:00:00.000Z"
-        }, {
-            "artist_id": 6,
-            "artist_name": "The Wild Sax Band",
-            "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-            "start_time": "2035-04-08T20:00:00.000Z"
-        }, {
-            "artist_id": 6,
-            "artist_name": "The Wild Sax Band",
-            "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-            "start_time": "2035-04-15T20:00:00.000Z"
-        }],
-        "past_shows_count": 1,
-        "upcoming_shows_count": 1,
-    }
-    data = list(filter(lambda d: d['id'] == venue_id, [data1, data2, data3]))[0]
+    venues = Venue.query.filter(Venue.id == venue_id).one_or_none()
+
+    if venues is None:
+        abort(404)
+
+    data = venues.serialize_with_shows_details
     return render_template('pages/show_venue.html', venue=data)
 
 
@@ -374,7 +359,10 @@ def create_venue_submission():
             state=venue_form.state.data,
             phone=venue_form.phone.data,
             facebook_link=venue_form.facebook_link.data,
-            image_link=venue_form.image_link.data)
+            image_link=venue_form.image_link.data,
+            website_link=venue_form.website_link.data,
+            seeking_talent=venue_form.seeking_talent.data,
+            seeking_description=venue_form.seeking_description.data)
 
         new_venue.add()
         # on successful db insert, flash success
@@ -394,6 +382,14 @@ def delete_venue(venue_id):
 
     # BONUS CHALLENGE: Implement a button to delete a Venue on a Venue Page, have it so that
     # clicking that button delete it from the db then redirect the user to the homepage
+    try:
+        venue_to_delete = Venue.query.filter(Venue.id == venue_id).one()
+        venue_to_delete.delete()
+        flash("Venue {0} has been deleted successfully".format(
+            venue_to_delete[0]['name']))
+    except NoResultFound:
+        abort(404)
+
     return None
 
 
