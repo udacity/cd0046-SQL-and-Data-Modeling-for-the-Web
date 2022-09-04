9bc2c827987538eb5b59b6a000ba76b9ba30ac8d
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2022-09-04 06:28:10 +0100

    chore: replace mock data with real artists data

--- app.py
+++ app.py
@@ -224,17 +224,8 @@ def delete_venue(venue_id):
 #  ----------------------------------------------------------------
 @app.route('/artists')
 def artists():
-  # TODO: replace with real data returned from querying the database
-  data=[{
-    "id": 4,
-    "name": "Guns N Petals",
-  }, {
-    "id": 5,
-    "name": "Matt Quevedo",
-  }, {
-    "id": 6,
-    "name": "The Wild Sax Band",
-  }]
+  # *DONE: replaced artists with real data returned from querying the database
+  data = Artist.query.all()
   return render_template('pages/artists.html', artists=data)
 
 @app.route('/artists/search', methods=['POST'])
