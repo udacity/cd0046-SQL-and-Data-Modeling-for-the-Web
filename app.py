77d35a8e80cdb2470f00b38d68d3c2bf00ff1ddc
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2022-09-04 05:11:51 +0100

    feat: create artist controller for form submission implemented

--- app.py
+++ app.py
@@ -197,7 +197,7 @@ def create_venue_submission():
                     image_link=image_link, seeking_talent=seeking_talent, seeking_description=seeking_description)
       db.session.add(venue)
       db.session.commit()
-  except  Exception as e:
+  except Exception as e:
       error = True
       db.session.rollback()
       print('Caught Exception: ', e)
@@ -397,13 +397,39 @@ def create_artist_form():
 @app.route('/artists/create', methods=['POST'])
 def create_artist_submission():
   # called upon submitting the new artist listing form
-  # TODO: insert form data as a new Venue record in the db, instead
-  # TODO: modify data to be the data object returned from db insertion
-
-  # on successful db insert, flash success
-  flash('Artist ' + request.form['name'] + ' was successfully listed!')
-  # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Artist ' + data.name + ' could not be listed.')
+  error = False
+  try:
+      name = request.form['name']
+      city = request.form['city']
+      state = request.form['state']
+      phone = request.form['phone']
+      image_link = request.form['image_link']
+      facebook_link = request.form['facebook_link']
+      genres = request.form.getlist('genres')
+      website = request.form['website_link']
+      seeking_venue = True if 'seeking_venue' in request.form else False
+      seeking_description = request.form['seeking_description']
+  # *DONE: insert form data as a new Venue record in the db, instead
+  # *DONE: modify data to be the data object returned from db insertion
+      artist = Artist(name=name, city=city, state=state, phone=phone,
+                      image_link=image_link, facebook_link=facebook_link,
+                      genres=genres, website=website, seeking_venue=seeking_venue,
+                      seeking_description=seeking_description)
+      db.session.add(artist)
+      db.session.commit()
+  except Exception as e:
+      error = True
+      db.session.rollback()
+      print('Caught Exception: ', e)
+      print(sys.exc_info())
+  finally:
+      db.session.close()
+  if error:
+  # *DONE: on unsuccessful db insert, flash an error instead.
+      flash('An error occurred. Artist ' +
+            request.form['name'] + ' could not be listed.')
+  else:
+      flash('Artist ' + request.form['name'] + ' was successfully listed!')
   return render_template('pages/home.html')
 
 
