0a321f170c39e55595dcd1d43a7396d9e40ccbeb
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2022-09-04 06:56:31 +0100

    chore: artist search controller with case insensitivity

--- app.py
+++ app.py
@@ -236,18 +236,24 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
+  # *DONE: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
   # search for "band" should return "The Wild Sax Band".
-  response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
-  }
-  return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
+  response={}
+  search_term=request.form.get('search_term', '')
+  artists = Artist.query.filter(
+    Artist.name.ilike(f"%{search_term}%")
+  ).all()
+  response["count"] = len(artists)
+  response["data"] = []
+  for artist in artists:
+    artistObj = {
+      "id": artist.id,
+      "name": artist.name,
+      "num_upcoming_shows": Show.query.filter_by(artist_id=artist.id).filter(Show.start_time > datetime.now()).count()
+    }
+    response["data"].append(artistObj)
+  return render_template('pages/search_artists.html', results=response, search_term=search_term)
 
 @app.route('/artists/<int:artist_id>')
 def show_artist(artist_id):
