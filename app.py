dd7a415f0551d767ea49ae8261e7f52b1b90112f
baronmdw <58744855+baronmdw@users.noreply.github.com>
2023-02-22 08:52:12 +0100

    setup successful

--- app.py
+++ app.py
@@ -6,12 +6,15 @@ import json
 import dateutil.parser
 import babel
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
+from jinja2.utils import markupsafe 
+markupsafe.Markup()
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
+from markupsafe import Markup
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
--- config.py
+++ config.py
@@ -10,4 +10,4 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Phantom92@localhost:5432/todoapp'
